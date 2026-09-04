13d996247f744d73e007b742f73f02c6c1977cfb
c20245727duay-lab <c20245168.senayo@csav.edu.ph>
2026-09-04 16:02:50 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 console.log("Hello, World!");
 console.log("From script.js file.");
 
+const myName = "Kimberly";
+let age = 20;
+let number = "09509783567";
+let address = "Silay City";
+let addnumber = 67;
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age} years old`);
+console.log(`number: ${number}`);
+console.log(`address: ${address}`);
+console.log(67);
+
+    function greet(greetings,name = []) {
+    if (Array.isArray(name)) {
+        name = name.join(", ");
+    return `${greetings} sa enyu, ${name}`;
+}
+    }
+console.log(greet("mayung aga", ["Kimberly", "Bai", "Soy", "Tol", "Erp"]));
