186652b4da89f824f0bdd1e0ea8c007d9cda21d2
c20245727duay-lab <c20245168.senayo@csav.edu.ph>
2026-09-04 16:35:41 +0800

    querrySelector

--- script.js
+++ script.js
@@ -20,3 +20,12 @@ console.log(67);
 }
     }
 console.log(greet("mayung aga", ["Kimberly", "Bai", "Soy", "Tol", "Erp"]));
+
+const heading = document.querySelector("h1");
+console.log(heading);
+
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading);
+
+const serviceHeading = document.querySelector("#services h2");
+console.log(serviceHeading);
