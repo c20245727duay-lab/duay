d8079b3b1cc3c2c64dee04ca9412972f998a5197
c20245727duay-lab <c20245168.senayo@csav.edu.ph>
2026-09-04 17:32:40 +0800

    addEventListener

--- script.js
+++ script.js
@@ -26,8 +26,12 @@ heading.textContent = "Philip Duay";
 heading.style.color = "purple";
 heading.style.backgroundColor = "yellow";
 heading.style.fontSize = "60px";
-
 console.log(heading);
+
+heading.addEventListener("click", function (){
+    heading.style.color = "red";
+    console.log("Heading was clicked!");
+});
 
 const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading);
@@ -35,4 +39,21 @@ console.log(contactHeading);
 const serviceHeading = document.querySelector("#services h2");
 console.log(serviceHeading);
 
+const toggleButton = document.querySelector("#switch");
+const body = document.querySelector("body");
+let isOff = false;
 
+
+toggleButton.addEventListener("click", function () {
+   isOff = !isOff;
+
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+
+})
